48b77d7ddae0a3cfacb696198084111a73ddee1c
mose <mose@mose.com>
2014-05-06 21:04:27 +0800

    make curl less verbose

--- install-zsh.sh
+++ install-zsh.sh
@@ -8,9 +8,12 @@ if ! command -v curl &> /dev/null; then
   fi
 fi
 
-curl -o $HOME/.bash-docker \
+curl -s -o $HOME/.bash-docker \
   https://raw.githubusercontent.com/mose/docker-tools/master/.bash-docker
 
 if ! grep '\.bash-docker' $HOME/.zshrc &> /dev/null; then
   echo "source $HOME/.bash-docker" >> $HOME/.zshrc
 fi
+
+echo "docker-tools installed."
+echo "type `docker-tools` to see the list of commands."
